58ac0499dcb47b6e00139aad0b8e48ff07358507
adyarabiat <ady.arabiat@gmail.com>
2020-11-09 19:28:27 +0400

    adding the React laziy loading commit

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,10 +1,12 @@
-import React, { Component } from "react";
+import React, { Component, Suspense } from "react";
 import { NavLink, Route, Switch, Redirect } from "react-router-dom";
 
 import styles from "./Blog.module.css";
 import Posts from "../../containers/Blog/Posts/Posts";
-import NewPost from "../../containers/Blog/NewPost/NewPost";
-
+// import NewPost from "../../containers/Blog/NewPost/NewPost";
+const NewPost = React.lazy(() => {
+  return import("../../containers/Blog/NewPost/NewPost");
+});
 class Blog extends Component {
   state = {
     auth: true,
@@ -31,7 +33,14 @@ class Blog extends Component {
         {/* Now here we can use this to navigate between the pages by adding it like this as component this on this path ... but the problem here is we are reloading the page everytime we do this , But if we add link to="the path which /" for example it will not load so if i check the refresh will find it that it will not refresh  */}
         <Switch>
           {this.state.auth ? (
-            <Route path="/new-post" exact component={NewPost} />
+            <Route
+              path="/new-post"
+              render={() => (
+                <Suspense fallback={<div>Loading....</div>}>
+                  <NewPost />
+                </Suspense>
+              )}
+            />
           ) : null}
           <Route path="/posts" component={Posts} />
 
